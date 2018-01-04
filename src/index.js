6bc24d4d6a487b08b4613767f599a32b2a53d1da
YanPanMichael <yanpancsu@icloud.com>
2018-01-04 23:55:19 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+class Board extends React.Component {
+    renderSquare(i) {
+      return <Square value={i} />;
+    }
+
+
+    class Square extends React.Component {
+        render() {
+          return (
+            <button className="square">
+              {this.props.value}
+            </button>
+          );
+        }
+      }
+      
